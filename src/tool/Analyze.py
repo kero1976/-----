281613bd81895d5a1759ec3034b04530decd6ba1
kero1976 <kero1976@outlook.jp>
2022-11-07 09:02:49 +0900

    フォーマットを修正

--- src/tool/Analyze.py
+++ src/tool/Analyze.py
@@ -1,5 +1,7 @@
 from logging import getLogger
 from tool.Execute import execute
+import unicodedata
+
 logger = getLogger(__name__)
 
 
@@ -10,8 +12,31 @@ class analyze():
 
     def head(self):
         result = []
-        result.append('分割区分:{}'.format(self.exe.get(1)[0]))
-        result.append('レコード区分:{}'.format(self.exe.get(1)[0]))
-        result.append('運用モード:{}'.format(self.exe.get(1)[0]))
+        result.append(self.format('分割区分', 1))
+        result.append(self.format('レコード区分', 1))
+        result.append(self.format('運用モード', 1))
+        result.append(self.format('発信VANセンターコード', 12))
+        result.append(self.format('発信センターコード', 12))
+        result.append(self.format('発信者コード', 12))
+        result.append(self.format('受信VANセンターコード', 12))
+        result.append(self.format('受信センターコード', 12))
+        result.append(self.format('受信者コード', 12))
+        result.append(self.format('[BPID]機関コード', 4))
+        result.append(self.format('[BPID]サブ機関コード', 2))
+        result.append(self.format('[BPID]バージョン', 2))
 
+        return result
+
+    def format(self, name, size):
+        """
+        全角が含む場合でも桁そろえ
+        https://mulberrytassel.com/python-practice-zenhan/
+        """
+        data = self.exe.get(size)
+        count = 0
+        for c in name:
+            if unicodedata.east_asian_width(c) in 'FWA':
+                count += 1
+        col = 20 - count
+        result = '{:{width}s}:{}({})'.format(name, data[0], data[1], width=col)
         return result
